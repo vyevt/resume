b5b9e4ad924176fbe37cc67e3e773e3d74841abd
Volodymyr Yevtushenko <wladimir.yetv@gmail.com>
2023-04-28 08:39:55 +0300

    add close button

--- src/app/feature/dialog/dialog.component.ts
+++ src/app/feature/dialog/dialog.component.ts
@@ -6,7 +6,9 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
   selector: 'app-dialog',
   styles: [`
  :host{
+    display:flex;
     padding:20px;
+    flex-direction:column;
                 height:fit-content
   }
   h1{
@@ -19,9 +21,23 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
   ul{
     margin:20px;
   }
+  .close{
+    display:flex;
+width:40px;
+justify-content:center;
+align-self:flex-end;
+align-items:center;
+color:#fafafa;
+font-size:16px;
+height:40px;
+border-radius:5px;
+cursor:pointer;
+background-color:red
+  }
 `],
   template: `
-    <h1 mat-dialog-title > {{ data.title }}</h1>
+   <div class="close" (click)=onNoClick()  > x </div>
+   <h1 mat-dialog-title > {{ data.title }}</h1>
     <ul>
       <div *ngFor="let task of data.tasks index as i" >
         -{{ task }}
